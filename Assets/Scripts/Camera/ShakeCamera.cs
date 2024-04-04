8bc418f44121248384c10f4d15a1e6e60202bfc4
Garioo <marius@qvarnstrom.dk>
2024-04-04 11:52:45 +0200

    Camera shake

--- Assets/Scripts/Camera/ShakeCamera.cs
+++ Assets/Scripts/Camera/ShakeCamera.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Video;
 
 public class ShakeCamera : MonoBehaviour
 {
@@ -7,10 +8,13 @@ public class ShakeCamera : MonoBehaviour
     public float shakeAmount = 0.7f;
     public float decreaseFactor = 1.0f;
     public CinemachineVirtualCamera virtualCamera;
+    public VideoPlayer initialVideoPlayer; // Add this variable to reference the initial VideoPlayer component
+    public VideoPlayer shakeVideoPlayer; // Add this variable to reference the VideoPlayer component to switch to
     bool shakeDone;
 
     private Vector3 originalPos;
     private float shakeDuration = 0f;
+    private bool initialVideoPlayed = false;
 
     void Start()
     {
@@ -21,14 +25,38 @@ public class ShakeCamera : MonoBehaviour
             return;
         }
         originalPos = virtualCamera.transform.localPosition;
+
+        // Play the initial video
+        if (initialVideoPlayer != null)
+        {
+            initialVideoPlayer.Play();
+        }
     }
 
     void Update()
     {
+        if (!initialVideoPlayed && initialVideoPlayer != null && initialVideoPlayer.isPlaying)
+        {
+            // Check if the initial video is still playing
+            return;
+        }
+        else
+        {
+            initialVideoPlayed = true;
+        }
+
         if (Input.acceleration.sqrMagnitude >= 3f && Input.acceleration.sqrMagnitude < 30f) // Adjust this value as needed for your desired sensitivity
         {
             Shake();
             VibrationManager.Vibrate(50); // Vibrate for 50 milliseconds
+            
+            // Switch to the shake video when shaking detected
+            if(shakeVideoPlayer != null)
+            {
+                shakeVideoPlayer.Play();
+                Debug.Log("Shake Video Playing");
+                Object.Destroy(initialVideoPlayer.gameObject);
+            }
         }
         else if (Input.acceleration.sqrMagnitude >= 30f && !shakeDone) // Adjust this value as needed for your desired sensitivity
         {
