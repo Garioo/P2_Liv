15c9a6f7601da6f4db070c50453fabe96077030a
Garioo <marius@qvarnstrom.dk>
2024-04-15 11:11:10 +0200

    Changes til lyd

---
+++ Assets/Scripts/Managers/WaypointManager/LivCollider.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LivCollider : MonoBehaviour
+{
+    public VenPath VenPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           Debug.Log("Collided with Liv");
+              VenPath.VenWalk = true;
+            
+        }
+    }
+}
